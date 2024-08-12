c3e3f0c3b702377cdb5a591c308ccc84e74753a5
sarthak goyal <sarthakgoyal911@gmail.com>
2024-08-12 16:22:21 +0530

    day1

---
+++ day1/datatype.js
@@ -0,0 +1,3 @@
+console.log(typeof 'hello')//string
+console.log(typeof true)//boolean
+console.log(typeof 10)//number
---
+++ day1/variable.js
@@ -0,0 +1,35 @@
+//Variable declaration
+var string = 'HelloWorld';
+var boolean = true;
+var undefined;
+var null_type = null;
+
+
+//declare 4 variables without assigning values
+var Variable1;
+var Variable2;
+var Variable3;
+var Variable4;
+
+//declare 4 variables with assigning values
+var str= 'hello'
+var num = 10
+var empty = null
+var bool= true
+
+//Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstName = 'Sarthak'
+let lastName = 'Goyal'
+let isMarried = false
+let country = 'India'
+let age = 23
+
+//Declare variables to store your first name, last name, marital status, country and age in a single line
+let firstname='Sarthak', lastname='Goyal',ismarried=false,Country='India',Age=23;
+
+//Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+let myAge=23
+let yourAge =25
+
+console.log('I am',myAge,'years old');
+console.log('You are',yourAge,'years old');
